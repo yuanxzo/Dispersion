918dba32a848a745188237382f1ef6a07f6f76f5
Bo YANG <b.yang@petalmail.com>
2022-11-24 16:23:18 +0800

    Update Dispersion.f90

--- src/Dispersion.f90
+++ src/Dispersion.f90
@@ -1,4 +1,4 @@
-!  Copyright (C) <2021>  <Bo Yang. Email:yuanxzo@qq.com>
+!  Copyright (C) <2021>  <Bo Yang. Email: b.yang@petalmail.com>
 !  
 !  This program is free software: you can redistribute it and/or modify
 !  it under the terms of the GNU General Public License as published by
