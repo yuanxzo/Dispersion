f6b8dd3141d93412ec665bab2eef93e69740b9bd
Bo YANG <seism.yang@foxmail.com>
2023-03-19 21:46:04 +0800

    Update Dispersion.f90

--- src/Dispersion.f90
+++ src/Dispersion.f90
@@ -1,4 +1,4 @@
-!  Copyright (C) <2021>  <Bo Yang. Email: b.yang@petalmail.com>
+!  Copyright (C) 2021  Bo Yang (seism.yang@foxmail.com)
 !  
 !  This program is free software: you can redistribute it and/or modify
 !  it under the terms of the GNU General Public License as published by
